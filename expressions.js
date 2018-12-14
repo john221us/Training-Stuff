534379a03b4e979351eeb0a0b17b5f41cb2836fd
john221us <45828631+john221us@users.noreply.github.com>
2018-12-14 08:25:05 -0800

    Learning files for Intro to Javascript

---
+++ expressions.js
@@ -0,0 +1,20 @@
+// This is bad
+a;
+
+// Types of expressions
+// ******************************
+
+// 1. Variable Declarations
+let a;
+
+// Perform an evaluation that rturns a single value
+// b + c
+
+let b = 3;
+let c = 2;
+// Three expressions in here ... can you find them?
+let a = b + c;
+
+//1. let a ... variable delcaration
+//2. perform an eval b + c
+// 3. result assigned to a
---
+++ variables.js
@@ -0,0 +1,15 @@
+// let x;
+// console.log(x);
+let x = 7;
+x = 6;
+x = 5;
+x = 4;
+let y = 3;
+let z = x + y;
+console.log('answer: ' + z);
+
+// let let = 8;
+let firstNumber = 7;
+let secondNumber = 3;
+let firstName = 'Bob';
+let zipCode = '95677';
